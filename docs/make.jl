a4b0d6deec28eab18faacd5acbf24c25c7cc2e72
Jie Kang <70739077+kanji709@users.noreply.github.com>
2021-06-11 19:53:45 +1200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,6 @@
 using Documenter, SimGBS
 
 makedocs(sitename = "SimGBS.jl", modules = [SimGBS], pages = ["Home" => "index.md"])
-
-deploydocs(repo = "github.com/kanji709/SimGBS.jl.git")
+deploydocs(
+  repo = "github.com/kanji709/SimGBS.jl.git",
+)
