bf2f639d73af643a627be56e44245133f8facfa1
kanji709 <kanji709@users.noreply.github.com>
2021-06-14 00:42:10 +0000

    Format .jl files

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,6 @@
 using Documenter, SimGBS
 
 
-makedocs(
- modules = [SimGBS], 
- sitename = "SimGBS.jl",
- pages = ["Home" => "index.md"]
-)
+makedocs(modules = [SimGBS], sitename = "SimGBS.jl", pages = ["Home" => "index.md"])
 
 deploydocs(repo = "github.com/kanji709/SimGBS.jl.git")
